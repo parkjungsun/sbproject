1af52d0ece206a496960278bf0a4551702f29340
parkjungsun <pjss7373@gmail.com>
2023-02-13 23:07:27 +0900

    first commit

---
+++ src/main/java/com/pjsun/sbproject/SbprojectApplication.java
@@ -0,0 +1,13 @@
+package com.pjsun.sbproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbprojectApplication.class, args);
+	}
+
+}
